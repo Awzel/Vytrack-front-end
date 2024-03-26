ca8d5eb84da85142b6fc9b0fa9f71b90584a27fe
Awzel <awzal0515@gmail.com>
2024-03-25 20:09:12 -0400

    Updated forgotPassword

--- src/test/java/com/vytrack/stepdefs/LoginStepDef.java
+++ src/test/java/com/vytrack/stepdefs/LoginStepDef.java
@@ -1,5 +1,6 @@
 package com.vytrack.stepdefs;
 
+import com.vytrack.pages.ForgotPasswordPage;
 import com.vytrack.pages.LoginPage;
 import static com.vytrack.utils.BrowserUtil.*;
 
@@ -9,11 +10,13 @@ import com.vytrack.utils.JsonReader;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.velocity.runtime.directive.contrib.For;
 import org.junit.Assert;
 
 public class LoginStepDef {
 
     LoginPage loginPage = new LoginPage();
+    ForgotPasswordPage forgotPasswordPage = new ForgotPasswordPage();
     @Given("the user logged in with {string} datatype as {string}")
     public void the_user_logged_in_with_datatype_as(String dataType, String userType) {
         loginPage.login(dataType,userType);
@@ -40,28 +43,28 @@ public class LoginStepDef {
 
     @When("I click on 'Forgot your password?' button")
     public void i_click_on_forgot_your_password_button() {
-        loginPage.resetPasswordLink.click();
+        BrowserUtil.click(forgotPasswordPage.resetPasswordLink);
     }
 
     @When("I enter {string} Username or Email")
     public void i_enter_username_or_email(String dataType) {
         if (dataType.equalsIgnoreCase("positive")) {
-            loginPage.userName.sendKeys(JsonReader.getSingleString("driver","username","positive"));
+            forgotPasswordPage.usernameOrEmailInput.sendKeys(JsonReader.getSingleString("driver","username","positive"));
         } else if (dataType.equalsIgnoreCase("negative")) {
-            loginPage.userName.sendKeys(JsonReader.getSingleString("driver","username","negative"));
+            forgotPasswordPage.usernameOrEmailInput.sendKeys(JsonReader.getSingleString("driver","username","negative"));
         }
-        loginPage.requestButton.click();
+        BrowserUtil.click(forgotPasswordPage.requestButton);
     }
 
 
     @Then("I should get success message")
     public void i_should_get_success_message() {
-        loginPage.forgotPasswordSuccessMsg.isDisplayed();
+        forgotPasswordPage.forgotPasswordSuccessMsg.isDisplayed();
     }
 
     @Then("I should get error message")
     public void i_should_get_error_message() {
-        loginPage.forgotPasswordFailedMsg.isDisplayed();
+        forgotPasswordPage.forgotPasswordFailedMsg.isDisplayed();
     }
 
 }
