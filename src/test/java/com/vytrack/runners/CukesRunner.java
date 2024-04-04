36e4c8a4e4fbb7a1c1db007fdc2129310e501c9c
flora <afeiluola@yahoo.com>
2024-04-04 12:13:48 -0400

    truck driver reset

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/stepdefs",
         dryRun = false,
-        tags = "@smoke",
+        tags = "@odometer-info",
         publish = true //generating a report with public link
 )
 
